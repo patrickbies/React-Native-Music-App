eb7ffc55d4ee9714372bbcb2c57f702d526f09ff
patrickbies <patjbies@gmail.com>
2024-11-27 10:18:53 -0500

    many UI changes - begin algorithm next

--- components/post/PostPage.tsx
+++ components/post/PostPage.tsx
@@ -1,34 +1,100 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Id } from '@/convex/_generated/dataModel';
+import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Id } from "@/convex/_generated/dataModel";
+import PostFooter from "./PostFooter";
+import { Colors } from "@/constants/Colors";
+import Animated from "react-native-reanimated";
+import { Image } from "expo-image";
 
-type posts = {
-  _id: Id<"posts">;
-  _creationTime: number;
-  createdAt: number;
-  userId: Id<"users">;
-  gifBackground: boolean;
-  name: string;
-  mediaUrl: string;
-  shares: number;
-  saves: number;
-  likes: number;
-  mediaId: string;
-}[];
+type posts =
+  | {
+      _id: Id<"posts">;
+      _creationTime: number;
+      createdAt: number;
+      userId: Id<"users">;
+      gifBackground: boolean;
+      name: string;
+      mediaUrl: string;
+      shares: number;
+      saves: number;
+      likes: number;
+      mediaId: string;
+    }[]
+  | undefined;
 
 type PostPage = {
   posts: posts;
   index: number;
-}
+  vis: boolean;
+  setIndex: React.Dispatch<React.SetStateAction<number>>;
+};
 
-const PostPage = ({posts, index} : PostPage) => {
+const { width: screenWidth, height: screenHeight } = Dimensions.get("screen");
+
+const PostPage = ({
+  posts,
+  index,
+  setIndex,
+  vis,
+}: PostPage) => {
+  if (!posts || !vis) return null;
+
+  const [paused, setPaused] = useState(false);
+  const [liked, setLiked] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const [time, setTime] = useState(0);
+
   return (
-    <View>
-      <Text>PostPage</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={posts}
+        style={{ height: screenHeight * 0.2 }}
+        renderItem={({item}) => (
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={item.mediaUrl} />
+          </View>
+        )}
+        viewabilityConfig={{ itemVisiblePercentThreshold: 100 }}
+        onViewableItemsChanged={(e) =>
+          e.viewableItems[0] && setIndex(e.viewableItems[0].index!)
+        }
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        pagingEnabled
+      />
+      <PostFooter
+        time={time}
+        setTime={setTime}
+        duration={30}
+        paused={paused}
+        setPaused={setPaused}
+        hasPrev={!!index}
+        hasNext={index < posts.length - 1}
+        liked={liked}
+        saved={saved}
+        setLiked={setLiked}
+        setSaved={setSaved}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default PostPage
+export default PostPage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.backgroundColor,
+  },
+  imageContainer: {
+    width: screenWidth,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "90%",
+    aspectRatio: 1,
+    backgroundColor: "#333",
+    borderRadius: 10,
+  },
+});
--- components/profile/ProfilePage.tsx
+++ components/profile/ProfilePage.tsx
@@ -1,25 +1,62 @@
 import {
+  Dimensions,
   FlatList,
   LayoutRectangle,
+  Modal,
+  Pressable,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useRef, useState } from "react";
 import ProfileHeader from "./ProfileHeader";
 import ProfileTabs from "./ProfileTabs";
 import { api } from "@/convex/_generated/api";
 import { Colors } from "@/constants/Colors";
 import { Image } from "expo-image";
-import { useSharedValue } from "react-native-reanimated";
+import Animated, {
+  measure,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 import { defaultStyles } from "@/constants/Styles";
+import { Id } from "@/convex/_generated/dataModel";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import PostPage from "../post/PostPage";
 
 type ProfilePage = {
   userData: typeof api.tasks.queryUser._returnType;
 };
 
+type post = {
+  _id: Id<"posts">;
+  _creationTime: number;
+  createdAt: number;
+  userId: Id<"users">;
+  gifBackground: boolean;
+  name: string;
+  mediaUrl: string;
+  shares: number;
+  saves: number;
+  likes: number;
+  mediaId: string;
+};
+
+const { width: sw, height: sh } = Dimensions.get("screen");
+const animateDuration = 500;
+
 const ProfilePage = ({ userData }: ProfilePage) => {
+  const refs = useRef<Record<Id<"posts">, View>>({});
+  const [index, setIndex] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+
   const dim = useSharedValue<LayoutRectangle>({
     width: 0,
     height: 0,
@@ -27,31 +64,137 @@ const ProfilePage = ({ userData }: ProfilePage) => {
     y: 0,
   });
 
-  return (
-    <View style={{flex: 1}}>
-    <FlatList
-      data={userData?.userPosts}
-      renderItem={(e) => (
-        <TouchableOpacity
-          style={styles.preview}
-          onLayout={({ nativeEvent }) => (dim.value = nativeEvent.layout)}
-          onPress={() => {}} // Open current post
-        >
-          <Image source={e.item.mediaUrl} style={styles.image} />
-          <Text style={defaultStyles.pTextM}>{e.item.name}</Text>
-        </TouchableOpacity>
-      )}
-      style={styles.container}
-      numColumns={2}
-      ListHeaderComponent={() => (
-        <>
-          <ProfileHeader metadata={userData?.metadata} />
-          <ProfileTabs />
-        </>
-      )}
-    />
-    {/* Make the animation to fullscreen here: */}
+  const handlePress = (item: post, index: number) => {
+    setIndex(index);
 
+    refs.current[item._id].measureInWindow((x, y, width, height) => {
+      dim.value = {
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+      };
+      setModalVisible(true);
+      console.log(dim.value);
+
+      dim.value = withTiming(
+        { x: 0, y: 0, width: sw, height: sh },
+        { duration: animateDuration }
+      );
+    });
+  };
+
+  const handleClose = () => {
+    let fromSize = {
+      x: 0, y: 0, width: 0, height: 0
+    }
+    if(userData)
+    refs.current[userData.userPosts[index]._id].measureInWindow((x, y, width, height) => {
+      fromSize = {x: x, y: y, width: width, height: height}
+    })
+
+    dim.value = withTiming(
+      {
+        x: fromSize.x,
+        y: fromSize.y,
+        width: fromSize.width,
+        height: fromSize.height,
+      },
+      { duration: animateDuration },
+      () => runOnJS(setModalVisible)(false)
+    );
+  };
+
+  const animatedScreen = useAnimatedStyle(() => ({
+    width: dim.value.width,
+    height: dim.value.height,
+    top: dim.value.y,
+    left: dim.value.x,
+  }));
+
+  const pan = Gesture.Pan()
+    .minDistance(1)
+    .onStart((e) => {
+      if (e.translationY < e.translationX) return;
+    })
+    .onChange((e) => {
+      dim.value = {
+        x: e.translationX,
+        y: e.translationY,
+        width: dim.value.width,
+        height: dim.value.height,
+      };
+    })
+    .onEnd((e) => {
+      const dist =
+        e.translationX * e.translationX + e.translationY * e.translationY;
+      const vel = e.velocityX * e.velocityX + e.velocityY * e.velocityY;
+      if (dist > 1000 * 100 || vel > 1000 * 1000) {
+        runOnJS(handleClose)();
+        return;
+      }
+
+      dim.value = withTiming({
+        x: 0,
+        y: 0,
+        width: dim.value.width,
+        height: dim.value.height,
+      });
+    });
+
+  const renderItem = (item: post, index: number) => (
+    <TouchableOpacity
+      ref={(ref) => {
+        if (ref) refs.current[item._id] = ref;
+      }}
+      style={styles.preview}
+      onPress={() => handlePress(item, index)}
+    >
+      <Image source={item.mediaUrl} style={styles.image} />
+      <Text style={defaultStyles.pTextM}>{item.name}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={userData?.userPosts}
+        renderItem={({ item, index }) => renderItem(item, index)}
+        style={styles.container}
+        numColumns={2}
+        onLayout={(e) => {}}
+        ListHeaderComponent={() => (
+          <>
+            <ProfileHeader metadata={userData?.metadata} />
+            <ProfileTabs />
+          </>
+        )}
+      />
+      {/* Make the animation to fullscreen here: */}
+      <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="none" // Disable default animations
+        onRequestClose={() => handleClose()}
+      >
+        <GestureHandlerRootView>
+          <GestureDetector gesture={pan}>
+            <Animated.View
+              style={[
+                { backgroundColor: "red", position: "absolute" },
+                animatedScreen,
+              ]}
+            >
+              <PostPage
+                index={index}
+                posts={userData?.userPosts}
+                vis={modalVisible}
+                setIndex={setIndex}
+              />
+            </Animated.View>
+          </GestureDetector>
+        </GestureHandlerRootView>
+      </Modal>
     </View>
   );
 };
